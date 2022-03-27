a874d03837f9cd11ac26e3b3d4a6b98a31414cb4
master <master@gmail.com>
2022-03-27 12:45:53 +0800

    main

---
+++ Linux_Web/01day/threadpool/reactor_1.c
@@ -0,0 +1,195 @@
+//反应堆简单版
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include "wrap.h"
+
+#define _BUF_LEN_  1024
+#define _EVENT_SIZE_ 1024
+
+//全局epoll树的根
+int gepfd = 0;
+
+//事件驱动结构体
+typedef struct xx_event{
+    int fd;
+    int events;
+    void (*call_back)(int fd,int events,void *arg);
+    void *arg;
+    char buf[1024];
+    int buflen;
+    int epfd;
+}xevent;
+
+xevent myevents[_EVENT_SIZE_+1];
+
+void readData(int fd,int events,void *arg);
+
+//添加事件
+//eventadd(lfd,EPOLLIN,initAccept,&myevents[_EVENT_SIZE_-1],&myevents[_EVENT_SIZE_-1]);
+// void (*call_back)(int ,int ,void *)实际上  声明函数指针
+void eventadd(int fd,int events,void (*call_back)(int ,int ,void *),void *arg,xevent *ev)
+{
+    ev->fd = fd;
+    ev->events = events;
+    //ev->arg = arg;//代表结构体自己，可以通过arg得到结构体的所有信息
+    ev->call_back = call_back;
+
+    struct epoll_event epv;//结构体
+    epv.events = events;
+    epv.data.ptr = ev;//核心思想
+    epoll_ctl(gepfd,EPOLL_CTL_ADD,fd,&epv);//上树
+}
+
+//修改事件
+//eventset(fd,EPOLLOUT,senddata,arg,ev);
+void eventset(int fd,int events,void (*call_back)(int ,int ,void *),void *arg,xevent *ev)
+{
+    ev->fd = fd;
+    ev->events = events;
+    //ev->arg = arg;
+    ev->call_back = call_back;
+
+    struct epoll_event epv;
+    epv.events = events;
+    epv.data.ptr = ev;
+    epoll_ctl(gepfd,EPOLL_CTL_MOD,fd,&epv);//ÐÞ¸Ä
+}
+
+//删除事件
+void eventdel(xevent *ev,int fd,int events)
+{
+	printf("begin call %s\n",__FUNCTION__);
+
+    ev->fd = 0;
+    ev->events = 0;
+    ev->call_back = NULL;
+    memset(ev->buf,0x00,sizeof(ev->buf));
+    ev->buflen = 0;
+
+    struct epoll_event epv;
+    epv.data.ptr = NULL;
+    epv.events = events;
+    epoll_ctl(gepfd,EPOLL_CTL_DEL,fd,&epv);//ÏÂÊ÷
+}
+
+//发送数据
+void senddata(int fd,int events,void *arg)
+{
+    printf("begin call %s\n",__FUNCTION__);
+
+    xevent *ev = arg;
+    Write(fd,ev->buf,ev->buflen);
+    eventset(fd,EPOLLIN,readData,arg,ev);
+}
+
+//读数据
+void readData(int fd,int events,void *arg)
+{
+    printf("begin call %s\n",__FUNCTION__);
+    xevent *ev = arg;
+
+    ev->buflen = Read(fd,ev->buf,sizeof(ev->buf));
+    if(ev->buflen>0) //¶Áµ½Êý¾Ý
+	{	
+		//void eventset(int fd,int events,void (*call_back)(int ,int ,void *),void *arg,xevent *ev)
+        eventset(fd,EPOLLOUT,senddata,arg,ev);
+
+    }
+	else if(ev->buflen==0) //¶Ô·½¹Ø±ÕÁ¬½Ó
+	{
+        Close(fd);
+        eventdel(ev,fd,EPOLLIN);
+    }
+
+}
+//新连接处理
+void initAccept(int fd,int events,void *arg)
+{
+    printf("begin call %s,gepfd =%d\n",__FUNCTION__,gepfd);//__FUNCTION__ 函数名
+
+    int i;
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(addr);
+    int cfd = Accept(fd,(struct sockaddr*)&addr,&len);//是否会阻塞
+	
+	//查找数组中可用的位置
+    for(i = 0 ; i < _EVENT_SIZE_; i ++)
+	{
+        if(myevents[i].fd==0)
+		{
+            break;
+        }
+    }
+
+    //设置读事件 将读事件加到节点上 
+    eventadd(cfd,EPOLLIN,readData,&myevents[i],&myevents[i]);
+}
+
+int main(int argc,char *argv[])
+{
+	//创建socket套接字
+    int lfd = Socket(AF_INET,SOCK_STREAM,0);
+
+    //设置端口复用
+    int opt = 1;
+    setsockopt(lfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+
+	//绑定
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8888);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    Bind(lfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+    
+	//监听
+    Listen(lfd,128);
+
+	//创建epoll树的根节点
+    gepfd = epoll_create(1024);
+    printf("gepfd === %d\n",gepfd);
+
+    //传出参数，若监测到事件，将所有就绪的文件描述符从内核表中复制到该数组中
+    struct epoll_event events[1024];
+
+
+    //添加最初始事件， 将监听的文件描述符上树
+    eventadd(lfd,EPOLLIN,initAccept,&myevents[_EVENT_SIZE_],&myevents[_EVENT_SIZE_]);
+    //void eventadd(int fd,int events,void (*call_back)(int ,int ,void *),void *arg,xevent *ev)
+
+    while(1)
+	{
+        int nready = epoll_wait(gepfd,events,1024,-1);
+		if(nready<0) //调用epoll_wait失败
+		{
+			perr_exit("epoll_wait error");
+			
+		}
+        else if(nready>0) //调用epoll_wait 成功，返回有事件发生的文件描述符
+		{
+            int i = 0;
+            for(i=0; i < nready; i++)
+			{
+                xevent *xe = events[i].data.ptr;//取返回事件ptr指向结构体
+                printf("fd=%d\n",xe->fd);
+
+                if(xe->events & events[i].events)
+				{
+                    xe->call_back(xe->fd,xe->events,xe);//调用事件对应的回调函数
+                }
+            }
+        }
+    }
+
+	//关闭监听文件描述符
+	Close(lfd);
+
+    return 0;
+}
